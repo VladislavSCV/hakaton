d4791b042267bb5b83978839de8e8400762b8909
VLADislav314 <116951315+VladislavSCV@users.noreply.github.com>
2024-12-12 11:57:16 +0300

    Update transactions.sql

--- internal/database/transactions.sql
+++ internal/database/transactions.sql
@@ -1,27 +1,26 @@
+-- Таблица компаний
+CREATE TABLE companies (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Таблица пользователей
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     salt TEXT NOT NULL,
-    company_id INT NOT NULL,
+    company_id UUID NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_company FOREIGN KEY (company_id) REFERENCES companies (id) ON DELETE CASCADE
 );
 
--- Таблица компаний
-CREATE TABLE companies (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
---     api_key TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 -- Таблица игр
 CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     company_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,
     data TEXT,
@@ -32,23 +31,26 @@ CREATE TABLE games (
 
 -- Таблица изображений
 CREATE TABLE images (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     company_id UUID NOT NULL,
-    game_id INT NOT NULL,
+    game_id UUID NOT NULL,
     url TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_company FOREIGN KEY (company_id) REFERENCES companies (id) ON DELETE CASCADE,
     CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES games (id) ON DELETE CASCADE
 );
 
 -- Таблица игровых сессий
 CREATE TABLE game_sessions (
-    id SERIAL PRIMARY KEY,
-    game_id INT NOT NULL,
-    user_id INT NOT NULL,
+    id UUID PRIMARY KEY,
+    game_id UUID NOT NULL,
+    user_id UUID NOT NULL,
     session_data TEXT,
     started_at TIMESTAMP,
     ended_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES games (id) ON DELETE CASCADE,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
